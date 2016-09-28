6d03e82a0e11c56cc35d0b66928d7185cab5198b
휘연 조 <hwyn.cho@gmail.com>
2016-09-28 15:54:59 +0900

    Update commit

---
+++ DruidPushApp/FormMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace DruidPushApp
+{
+	partial class Form
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.ToolStripMenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+			this.textBox_URL = new System.Windows.Forms.TextBox();
+			this.label_URL = new System.Windows.Forms.Label();
+			this.button_OK = new System.Windows.Forms.Button();
+			this.button_Cancel = new System.Windows.Forms.Button();
+			this.contextMenuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "notifyIcon";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.BalloonTipClicked += new System.EventHandler(this.notifyIcon_BalloonTipClicked);
+			// 
+			// contextMenuStrip
+			// 
+			this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_Exit});
+			this.contextMenuStrip.Name = "contextMenuStrip";
+			this.contextMenuStrip.Size = new System.Drawing.Size(270, 86);
+			// 
+			// ToolStripMenuItem_Exit
+			// 
+			this.ToolStripMenuItem_Exit.Name = "ToolStripMenuItem_Exit";
+			this.ToolStripMenuItem_Exit.Size = new System.Drawing.Size(269, 38);
+			this.ToolStripMenuItem_Exit.Text = "종료";
+			this.ToolStripMenuItem_Exit.Click += new System.EventHandler(this.ToolStripMenuItem_Exit_Click);
+			// 
+			// textBox_URL
+			// 
+			this.textBox_URL.Font = new System.Drawing.Font("맑은 고딕", 10F);
+			this.textBox_URL.Location = new System.Drawing.Point(12, 49);
+			this.textBox_URL.Name = "textBox_URL";
+			this.textBox_URL.Size = new System.Drawing.Size(700, 43);
+			this.textBox_URL.TabIndex = 2;
+			// 
+			// label_URL
+			// 
+			this.label_URL.AutoSize = true;
+			this.label_URL.Font = new System.Drawing.Font("맑은 고딕", 10F);
+			this.label_URL.Location = new System.Drawing.Point(12, 9);
+			this.label_URL.Name = "label_URL";
+			this.label_URL.Size = new System.Drawing.Size(80, 37);
+			this.label_URL.TabIndex = 3;
+			this.label_URL.Text = "URL :";
+			// 
+			// button_OK
+			// 
+			this.button_OK.Location = new System.Drawing.Point(457, 111);
+			this.button_OK.Name = "button_OK";
+			this.button_OK.Size = new System.Drawing.Size(117, 56);
+			this.button_OK.TabIndex = 4;
+			this.button_OK.Text = "확인";
+			this.button_OK.UseVisualStyleBackColor = true;
+			this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+			// 
+			// button_Cancel
+			// 
+			this.button_Cancel.Location = new System.Drawing.Point(595, 111);
+			this.button_Cancel.Name = "button_Cancel";
+			this.button_Cancel.Size = new System.Drawing.Size(117, 56);
+			this.button_Cancel.TabIndex = 5;
+			this.button_Cancel.Text = "종료";
+			this.button_Cancel.UseVisualStyleBackColor = true;
+			this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+			// 
+			// Form
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(724, 179);
+			this.Controls.Add(this.button_Cancel);
+			this.Controls.Add(this.button_OK);
+			this.Controls.Add(this.label_URL);
+			this.Controls.Add(this.textBox_URL);
+			this.Font = new System.Drawing.Font("맑은 고딕", 10F);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "DruidPushApp";
+			this.contextMenuStrip.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Exit;
+		private System.Windows.Forms.TextBox textBox_URL;
+		private System.Windows.Forms.Label label_URL;
+		private System.Windows.Forms.Button button_OK;
+		private System.Windows.Forms.Button button_Cancel;
+	}
+}
+
---
+++ DruidPushApp/FormMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using HtmlAgilityPack;
+
+namespace DruidPushApp
+{
+	public partial class Form : System.Windows.Forms.Form
+	{
+		Thread thread;
+
+		String URL;			// 입력한 URL을 저장
+
+		int oldCount;		// 이전 게시물 갯수 저장
+		int newCount;		// 새 게시물 갯수 저장
+
+		public Form()
+		{
+			InitializeComponent();
+
+			MySetVariable();
+		}
+		
+		private void button_OK_Click(object sender, EventArgs e)
+		{
+			HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlAgilityPack.HtmlWeb();
+			HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+			HtmlAgilityPack.HtmlNode htmlNode;
+
+			URL = this.textBox_URL.Text;
+
+			htmlDocument = htmlWeb.Load(URL);
+			htmlNode = htmlDocument.DocumentNode.SelectNodes("//tbody//tr//th").First();
+
+			oldCount = Convert.ToInt32(htmlNode.InnerHtml);
+
+			this.Visible = false;
+			this.ShowInTaskbar = false;
+			this.WindowState = FormWindowState.Minimized;
+
+			// 알림 표시
+			notifyIcon.BalloonTipText = "백그라운드에서 실행 중 입니다.";
+			notifyIcon.ShowBalloonTip(3000);
+
+			// 스레드 실행
+			thread.Start();
+		}
+
+		private void button_Cancel_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void notifyIcon_BalloonTipClicked(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(URL);
+		}
+
+		private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e)
+		{
+			thread.Abort();
+
+			this.Visible = true;
+			this.ShowInTaskbar = true;
+			this.WindowState = FormWindowState.Normal;
+		}
+
+		/* 변수 초기화 함수 */
+		public void MySetVariable()
+		{
+			thread = new Thread(new ThreadStart(MyParser));
+
+			URL = "";
+			oldCount = 0;
+			newCount = 0;
+		}
+
+		/* HTML 소스 파싱 함수 */
+		public void MyParser()
+		{
+			HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlAgilityPack.HtmlWeb();
+			HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+			HtmlAgilityPack.HtmlNode htmlNode;
+			HtmlAgilityPack.HtmlNode htmlNode_Number;
+			HtmlAgilityPack.HtmlNode htmlNode_Title;
+			HtmlAgilityPack.HtmlNode htmlNode_Writer;
+
+			while (true)
+			{
+				htmlDocument = htmlWeb.Load(URL);
+				htmlNode = htmlDocument.DocumentNode.SelectNodes("//tbody//tr").First();
+				htmlNode_Number = htmlNode.SelectNodes(".//th").First();
+				htmlNode_Title = htmlNode.SelectNodes(".//td//a[@class='detail-title']").First();
+				htmlNode_Writer = htmlNode.SelectNodes(".//td[@class='text-center detail-username']").First();
+
+				newCount = Convert.ToInt32(htmlNode_Number.InnerHtml);
+
+				if (newCount > oldCount)
+				{
+					MyPushNotify(htmlNode_Writer.InnerHtml, htmlNode_Title.InnerHtml);
+					oldCount = newCount;
+				}
+			}
+		}
+
+		/* 알림 표시 함수 */
+		public void MyPushNotify(String writer, String title)
+		{
+			notifyIcon.BalloonTipText = "새 질문이 등록되었습니다.\n" + "작성자 : " + writer + "\n" + "제목 : " + title;
+			notifyIcon.ShowBalloonTip(5000);
+		}
+	}
+}
